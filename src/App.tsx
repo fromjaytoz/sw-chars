042aa2d7d01b49abb8ca721286fdaa6e0b71deec
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-26 01:20:58 +0800

    add styled-components as dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from "react";
-import { Button } from "antd";
+import { Button, Layout } from "antd";
+import styled from "styled-components";
 import "./App.css";
 
+const { Header, Footer, Content } = Layout;
+
+const HeaderView = styled(Header)``;
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Layout>
+      <HeaderView>
+        Header <Button>Test</Button>
+      </HeaderView>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
   </div>
 );
 
