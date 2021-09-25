24d5515b7d46307c4039fb6146e359bcad93b820
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-26 02:47:15 +0800

    create successful button render test @navbar

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,6 @@ import { render, screen } from "@testing-library/react";
 import Navbar from "./components/Navbar/Navbar";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test("renders navbar", async () => {
   render(<Navbar />);
   const linkElement = await screen.findByTestId("navbar");
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "antd";
 
 const Navbar = () => {
-  return <div data-testid="navbar"></div>;
+  return (
+    <div data-testid="navbar">
+      <Button data-testid="button">Test</Button>
+    </div>
+  );
 };
 
 export default Navbar;
