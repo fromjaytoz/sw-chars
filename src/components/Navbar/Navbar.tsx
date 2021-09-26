6ffb4ed830b2eb61f53389edef0dc2bfc3bbec94
fromjaytoz <76098411+fromjaytoz@users.noreply.github.com>
2021-09-26 09:29:07 +0800

    add form to App.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Typography } from "antd";
+import styled from "styled-components";
+const { Title } = Typography;
+
+const NavView = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
 
 const Navbar = () => {
   return (
-    <div data-testid="navbar">
-      <Button data-testid="button">Test</Button>
-    </div>
+    <NavView data-testid="navbar">
+      <h1>Test</h1>
+      <Button data-testid=" button">Test</Button>
+    </NavView>
   );
 };
 
